e0c41eae6c13ba259ff0342de49345f0fe868c56
Alex Lentz <alex.lentz0@gmail.com>
2017-08-07 04:54:42 +0000

    Added Paperclip gem for profile pictures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728050100) do
+ActiveRecord::Schema.define(version: 20170807045332) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -36,8 +36,12 @@ ActiveRecord::Schema.define(version: 20170728050100) do
     t.string   "email"
     t.string   "company"
     t.text     "description"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
