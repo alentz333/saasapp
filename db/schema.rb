02409b2ef9c634d76c5a5e4bf14394db6bd14aa3
Alex Lentz <alex.lentz0@gmail.com>
2017-08-17 04:19:08 +0000

    Added picture to sign in nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807045332) do
+ActiveRecord::Schema.define(version: 20170817025942) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
